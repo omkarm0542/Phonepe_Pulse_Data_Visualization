5b8ef71e5ca6426009d130fe07851f965d275a00
OMKAR MAURYA <123791884+omkarm0542@users.noreply.github.com>
2023-07-17 13:12:33 +0530

    Add files via upload

--- phone.py
+++ phone.py
@@ -1,7 +1,8 @@
 #pip install mysql-connector-python
 #pip install streamlit plotly mysql-connector-python
 #pip install streamlit
-#
+#pip install streamlit_extras
+
 import mysql.connector 
 import pandas as pd
 #import psycopg2
@@ -11,31 +12,18 @@ from PIL import Image
 from streamlit_option_menu import option_menu
 import plotly.express as px
 import pandas as pd
-
+import matplotlib.pyplot as plt
 import requests
-
+import geopandas as gpd
 # connect to the database
 import mysql.connector
 
 #establishing the connection
-conn = mysql.connector.connect(user='root', password='omkar123', host='127.0.0.1', database="phonepe_pulse")
+conn = mysql.connector.connect(user='root', password='omkar123', host='127.0.0.1', database="phonepe")
 
 # create a cursor object
 cursor = conn.cursor()
 
-# execute a SELECT statement
-#cursor.execute("SELECT * FROM agg_trans")
-
-# fetch all rows
-#rows = cursor.fetchall()
-
-# print the rows
-#for row in rows:
-  #print(row)
-
-# close the cursor and database connection
-#cursor.close()
-#conn.close()
 
 #with st.sidebar:
 SELECT = option_menu(
@@ -95,16 +83,16 @@ if SELECT == "Basic insights":
             
             #3
             
-    elif select=="Top 5 Transaction_Type based on Transaction_Amount":
-        cursor.execute("SELECT DISTINCT Transaction_Type, SUM(Transaction_Amount) AS Amount FROM agg_user GROUP BY Transaction_Type ORDER BY Amount DESC LIMIT 5");
-        df = pd.DataFrame(cursor.fetchall(),columns=['Transaction_Type','Transaction_Amount '])
-        col1,col2 = st.columns(2)
+    elif select == "Top 5 Transaction_Type based on Transaction_Amount":
+        cursor.execute("SELECT DISTINCT Transaction_Type, SUM(Transaction_Amount) AS Amount FROM agg_user GROUP BY Transaction_Type ORDER BY Amount DESC LIMIT 5")
+        df = pd.DataFrame(cursor.fetchall(), columns=['Transaction_Type', 'Transaction_Amount'])
+        col1, col2 = st.columns(2)
         with col1:
             st.write(df)
         with col2:
             st.title("Top 5 Transaction_Type based on Transaction_Amount")
-            st.bar_chart(data=df,x="Transaction_Type",y="Amount")
-            
+            st.bar_chart(data=df, y="Transaction_Type", x="Transaction_Amount")
+
             #4
             
     elif select=="Top 10 Registered-users based on States and District":
@@ -115,7 +103,7 @@ if SELECT == "Basic insights":
             st.write(df)
         with col2:
             st.title("Top 10 Registered-users based on States and District")
-            st.bar_chart(data=df,x="State",y="RegisteredUsers")
+            st.bar_chart(data=df,y="State",x="RegisteredUsers")
             
             #5
             
@@ -127,7 +115,7 @@ if SELECT == "Basic insights":
             st.write(df)
         with col2:
             st.title("Top 10 Districts based on states and Count of transaction")
-            st.bar_chart(data=df,x="States",y="Transaction_Count")
+            st.bar_chart(data=df,y="States",x="Transaction_Count")
             
             #6
             
@@ -139,7 +127,7 @@ if SELECT == "Basic insights":
             st.write(df)
         with col2:
             st.title("Least 10 Districts based on states and amount of transaction")
-            st.bar_chart(data=df,x="States",y="Transaction_Amount")
+            st.bar_chart(data=df,y="States",x="Transaction_Amount")
             
             #7
             
@@ -151,7 +139,7 @@ if SELECT == "Basic insights":
             st.write(df)
         with col2:
             st.title("List 10 Transaction_Count based on Districts and states")
-            st.bar_chart(data=df,x="States",y="Transaction_Count")
+            st.bar_chart(data=df,y="States",x="Transaction_Count")
             
             #8
              
@@ -163,7 +151,7 @@ if SELECT == "Basic insights":
             st.write(df)
         with col2:
             st.title("Top 10 RegisteredUsers based on states and District")
-            st.bar_chart(data=df,x="States",y="RegisteredUsers")
+            st.bar_chart(data=df,y="States",x="RegisteredUsers")
 
 
 #----------------Home----------------------#
@@ -174,6 +162,7 @@ cursor.execute("SELECT * FROM agg_trans")
 
 # fetch all rows
 rows = cursor.fetchall()
+from streamlit_extras.add_vertical_space import add_vertical_space
 
 if SELECT == "Home":
     col1,col2, = st.columns(2)
@@ -184,29 +173,137 @@ if SELECT == "Home":
     with col2:
         st.video("C:/Users/omkar/Downloads/phonepe photo/upi.mp4")
         
-        
-    df = pd.DataFrame(rows, columns=['States', 'Transaction_Year', 'Quarters', 'Transaction_Type', 'Transaction_Count','Transaction_Amount'])
-    fig = px.choropleth(df, locations="States", scope="asia", color="States", hover_name="States",
-        title="Live Geo Visualization of India")
-    st.plotly_chart(fig)
-
-
+    st.subheader(':blue[Registered Users Hotspots - States]')
 
 
     
+
+    
+      
+      
+    Data_Aggregated_Transaction_df= pd.read_csv(r'C:/Users/omkar/Downloads/phonepe photo/Data_Aggregated_Transaction_Table.csv')
+    Data_Aggregated_User_Summary_df= pd.read_csv(r'C:/Users/omkar/Downloads/phonepe photo/Data_Aggregated_User_Summary_Table.csv')
+    Data_Aggregated_User_df= pd.read_csv(r'C:/Users/omkar/Downloads/phonepe photo/Data_Aggregated_User_Table.csv')
+    Scatter_Geo_Dataset =  pd.read_csv(r'C:/Users/omkar/Downloads/phonepe photo/Data_Map_Districts_Longitude_Latitude.csv')
+    Coropleth_Dataset =  pd.read_csv(r'C:/Users/omkar/Downloads/phonepe photo/Data_Map_IndiaStates_TU.csv')
+    Data_Map_Transaction_df = pd.read_csv(r'C:/Users/omkar/Downloads/phonepe photo/Data_Map_Transaction_Table.csv')
+    Data_Map_User_Table= pd.read_csv(r'C:/Users/omkar/Downloads/phonepe photo/Data_Map_User_Table.csv')
+    Indian_States= pd.read_csv(r'C:/Users/omkar/Downloads/phonepe photo/Longitude_Latitude_State_Table.csv')
+    
+    c1,c2=st.columns(2)
+    with c1:
+        Year = st.selectbox(
+                'Please select the Year',
+                ('2018', '2019', '2020','2021','2022'))
+    with c2:
+        Quarter = st.selectbox(
+                'Please select the Quarter',
+                ('1', '2', '3','4'))
+    year=int(Year)
+    quarter=int(Quarter)
+    
+    Transaction_scatter_districts=Data_Map_Transaction_df.loc[(Data_Map_Transaction_df['Year'] == year ) & (Data_Map_Transaction_df['Quarter']==quarter) ].copy()
+    Transaction_Coropleth_States=Transaction_scatter_districts[Transaction_scatter_districts["State"] == "india"]
+    Transaction_scatter_districts.drop(Transaction_scatter_districts.index[(Transaction_scatter_districts["State"] == "india")],axis=0,inplace=True)
+    # Dynamic Scattergeo Data Generation
+    
+    Transaction_scatter_districts = Transaction_scatter_districts.sort_values(by=['Place_Name'], ascending=False)
+    Scatter_Geo_Dataset = Scatter_Geo_Dataset.sort_values(by=['District'], ascending=False) 
+    Total_Amount=[]
+    for i in Transaction_scatter_districts['Total_Amount']:
+        Total_Amount.append(i)
+    Scatter_Geo_Dataset['Total_Amount']=Total_Amount
+    Total_Transaction=[]
+    for i in Transaction_scatter_districts['Total_Transactions_count']:
+        Total_Transaction.append(i)
+    Scatter_Geo_Dataset['Total_Transactions']=Total_Transaction
+    Scatter_Geo_Dataset['Year_Quarter']=str(year)+'-Q'+str(quarter)
+    # Dynamic Coropleth
+    
+    Coropleth_Dataset = Coropleth_Dataset.sort_values(by=['state'], ascending=False)
+    Transaction_Coropleth_States = Transaction_Coropleth_States.sort_values(by=['Place_Name'], ascending=False)
+    Total_Amount=[]
+    for i in Transaction_Coropleth_States['Total_Amount']:
+        Total_Amount.append(i)
+    Coropleth_Dataset['Total_Amount']=Total_Amount
+    Total_Transaction=[]
+    for i in Transaction_Coropleth_States['Total_Transactions_count']:
+        Total_Transaction.append(i)
+    Coropleth_Dataset['Total_Transactions']=Total_Transaction 
     
     
     
     
+    #scatter plotting the states codes 
+    Indian_States = Indian_States.sort_values(by=['state'], ascending=False)
+    Indian_States['Registered_Users']=Coropleth_Dataset['Registered_Users']
+    Indian_States['Total_Amount']=Coropleth_Dataset['Total_Amount']
+    Indian_States['Total_Transactions']=Coropleth_Dataset['Total_Transactions']
+    Indian_States['Year_Quarter']=str(year)+'-Q'+str(quarter)
+    fig=px.scatter_geo(Indian_States,
+                        lon=Indian_States['Longitude'],
+                        lat=Indian_States['Latitude'],                                
+                        text = Indian_States['code'], #It will display district names on map
+                        hover_name="state", 
+                        hover_data=['Total_Amount',"Total_Transactions","Year_Quarter"],
+                        )
+    fig.update_traces(marker=dict(color="white" ,size=0.3))
+    fig.update_geos(fitbounds="locations", visible=False,)
+    # scatter plotting districts
+    Scatter_Geo_Dataset['col']=Scatter_Geo_Dataset['Total_Transactions']
+    fig1=px.scatter_geo(Scatter_Geo_Dataset,
+                        lon=Scatter_Geo_Dataset['Longitude'],
+                        lat=Scatter_Geo_Dataset['Latitude'],
+                        color=Scatter_Geo_Dataset['col'],
+                        size=Scatter_Geo_Dataset['Total_Transactions'],     
+                    #text = Scatter_Geo_Dataset['District'], #It will display district names on map
+                        hover_name="District", 
+                        hover_data=["State", "Total_Amount","Total_Transactions","Year_Quarter"],
+                        title='District',
+                        size_max=22)
     
-    
-    
-    
-    
-    
-    
-    
-    
+    fig1.update_traces(marker=dict(color="rebeccapurple" ,line_width=1))    #rebeccapurple
+#coropleth mapping india
+    fig_ch = px.choropleth(
+                        Coropleth_Dataset,
+                        geojson="https://gist.githubusercontent.com/jbrobst/56c13bbbf9d97d187fea01ca62ea5112/raw/e388c4cae20aa53cb5090210a42ebb9b765c0a36/india_states.geojson",
+                        featureidkey='properties.ST_NM',                
+                        locations='state',
+                        color="Total_Transactions",                                       
+                        )
+    fig_ch.update_geos(fitbounds="locations", visible=False,)
+#combining districts states and coropleth
+    fig_ch.add_trace( fig.data[0])
+    fig_ch.add_trace(fig1.data[0])
+    st.write("### **:blue[PhonePe India Map]**")
+    colT1,colT2 = st.columns([6,4])
+    with colT1:
+        st.plotly_chart(fig_ch, use_container_width=True)
+    with colT2:
+        st.info(
+        """
+        Details of Map:
+        - The darkness of the state color represents the total transactions
+        - The Size of the Circles represents the total transactions dictrict wise
+        - The bigger the Circle the higher the transactions
+        - Hover data will show the details like Total transactions, Total amount
+        """
+        )
+        st.info(
+        """
+        Important Observations:
+        - User can observe Transactions of PhonePe in both statewide and Districtwide.
+        - We can clearly see the states with highest transactions in the given year and quarter
+        - We get basic idea about transactions district wide
+        """
+        )
+# -----------------------------------------------FIGURE2 HIDDEN BARGRAPH------------------------------------------------------------------------
+    Coropleth_Dataset = Coropleth_Dataset.sort_values(by=['Total_Transactions'])
+    fig = px.bar(Coropleth_Dataset, x='state', y='Total_Transactions',title=str(year)+" Quarter-"+str(quarter))
+    with st.expander("See Bar graph for the same data"):
+        st.plotly_chart(fig, use_container_width=True)
+        st.info('**:blue[The above bar graph showing the increasing order of PhonePe Transactions according to the states of India, Here we can observe the top states with highest Transaction by looking at graph]**')
+
     
     
 #----------------About-----------------------#
